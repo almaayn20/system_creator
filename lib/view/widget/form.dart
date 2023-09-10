2eb09942f55e914733b0638838d0f148ddf17c4c
AhmedAl-Rashedi <119629431+AhmedAl-Rashedi@users.noreply.github.com>
2023-09-10 18:37:05 +0300

    Create form.dart

---
+++ lib/view/widget/form.dart
@@ -0,0 +1,52 @@
+Form formCustom(
+    {required final formKey, required final List<Widget> widgets}) {
+  return Form(
+      key: formKey,
+      child: Column(
+        children: widgets,
+      ),
+  );
+}
+
+
+// // الاستدعاء
+// formCustom(formKey: _formKey, widgets: listOfWidgets),
+
+
+// // Key
+// static final _formKey = GlobalKey<FormState>();
+
+
+// //  List of widget
+// final List<Widget> listOfWidgets = [
+//   textFormFieldCustom(validate: validate,controller: controller,  label: "lael", hint: "hint",obscure: false, kbtype: TextInputType.text,),
+//           Padding(padding: EdgeInsets.all(10.0)),
+//           textFormFieldCustom(validate: validate,controller: controller2,  label: "label", hint: "hint",obscure: true, kbtype: TextInputType.number,),
+//           Padding(padding: EdgeInsets.all(10.0)),
+//           textFormFieldCustom(validate: validate,controller: controller3,  label: "label", hint: "hint",obscure: true, kbtype: TextInputType.number,),
+//           Padding(padding: EdgeInsets.all(10.0)),
+//           elevatedButtonCustom(func: anyFunction, text: "submit"),
+//   ];
+
+
+
+// // دالة التحقق من حقول النص
+// static String? validate(value) {
+// if (value == null || value.isEmpty) {
+//                 return 'Please enter some text';
+//               }
+//               return null;
+// }
+
+
+
+
+// // عند ضغط الزر
+// static anyFunction() {
+//                   // اذا تم التحقق 
+//                 if (_formKey.currentState!.validate()) {
+//                    // اكتب الكود هنا
+//                 }
+// }
+
+
