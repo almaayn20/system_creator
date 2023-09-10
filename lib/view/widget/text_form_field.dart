a0ec6d6c1f074a4323397bcbcf473ec7d44dcfb9
AhmedAl-Rashedi <119629431+AhmedAl-Rashedi@users.noreply.github.com>
2023-09-10 18:38:32 +0300

    create text_form_field.dart

--- lib/view/widget/text_form_field.dart
+++ lib/view/widget/text_form_field.dart
@@ -1,4 +1,4 @@
-TextFormField textFormFieldCustom(
+static TextFormField textFormFieldCustom(
     {required String? Function(dynamic value) validate,
     required TextEditingController controller,
     required String label,
@@ -27,17 +27,18 @@ TextFormField textFormFieldCustom(
   );
 }
 
-// الاستدعاء
-textFormFieldCustom(validate: validate,controller: controller,  label: "label", hint: "hint",obscure: false, kbtype: TextInputType.text,)
 
 
-  
+// // الاستدعاء
+// textFormFieldCustom(validate: validate,controller: controller,  label: "label", hint: "hint",obscure: false, kbtype: TextInputType.text,)
 
-//  validate دالة ال 
 
-static String? validate(value) {
-if (value == null || value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-}
+
+// //  validate دالة ال 
+
+// static String? validate(value) {
+// if (value == null || value.isEmpty) {
+//                 return 'Please enter some text';
+//               }
+//               return null;
+// }
